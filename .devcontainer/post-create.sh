1c25c5ed88c3a2d338f812477d72e648f136b206
Michael Hopcroft <mhop@microsoft.com>
2024-09-11 23:16:06 +0000

    WIP: poetry

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # conda create -y --prefix ./.conda conda-forge::pip-tools conda-forge::ipython
-pip install ipython pip-tools
+pip install jupyter pipx
+pipx install poetry
+poetry install --no-root

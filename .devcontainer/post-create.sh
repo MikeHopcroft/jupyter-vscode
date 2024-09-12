2ddc12fa9ba7213bb774800a8dbf0ce4f66e317a
Michael Hopcroft <mhop@microsoft.com>
2024-09-12 16:50:50 +0000

    Another post-create attempt

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,5 +3,5 @@
 # pip install jupyter pipx
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
-python3 -m pipx install poetry==1.8.3
-python3 -m poetry install --no-root
+pipx install poetry==1.8.3
+poetry install --no-root

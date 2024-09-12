be1e9cf0ce9852cb13ec2f8bebd3cd5cf05714d3
Michael Hopcroft <mhop@microsoft.com>
2024-09-12 16:00:30 +0000

    Another post-create fix

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # conda create -y --prefix ./.conda conda-forge::pip-tools conda-forge::ipython
-pip install jupyter pipx
-pipx install poetry
-pipx ensurepath
+# pip install jupyter pipx
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+pipx install poetry=1.8.3
+# pipx ensurepath
 poetry install --no-root

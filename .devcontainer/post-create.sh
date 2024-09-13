405dfc8664ab4ee5a130ef8aeea769dbd95e43f7
Michael Hopcroft <mhop@microsoft.com>
2024-09-13 22:37:30 +0000

    Another post-create idea

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pipx ensurepath
-source ~/.bashrc
-poetry install --no-root
+# source ~/.bashrc
+# poetry install --no-root
 # /home/vscode/.local/bin/poetry install --no-root
--- .devcontainer/update-content.sh
+++ .devcontainer/update-content.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 pip install pipx
 pipx install poetry==1.8.3
+source ~/.bashrc
+poetry install --no-root
